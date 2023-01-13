cd56efdf19295535ccb1173a9ed0c693c1698836
Adam Akers <adamrakers@gmail.com>
2023-01-12 21:09:30 -0700

    added js script to toggle nav

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,8 @@
-const adam = {
-  
+const hamMenuEl = document.querySelector('.hamburger-menu');
+
+function navToggle() {
+  const navMenuEl = document.querySelector('.page-links');
+  navMenuEl.classList.toggle('show');
 }
+
+hamMenuEl.addEventListener('click', navToggle);
